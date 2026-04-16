b23457854e9eed0a692cce33709de3718c189681
Ebenezer Tiewei <ebenezertiewei@gmail.com>
2026-04-16 21:06:17 +0100

    updated

--- frontend/src/Components/ProjectsSection.tsx
+++ frontend/src/Components/ProjectsSection.tsx
@@ -21,8 +21,8 @@ const projects = [
 		description:
 			"A full-stack tax calculation app with real-time updates. so you can calculate your taxes instantly.",
 		tech: ["React", "Tailwind", "TypeScript", "Node.js", "MongoDB"],
-		live: "https://taxlator-v2.vercel.app/",
-		github: "https://github.com/ebenezerkingv2/taxlator-v2",
+		live: "https://taxlatorv2.vercel.app/",
+		github: "https://github.com/ebenezerkingv2/taxlator_v2",
 	},
 	{
 		images: {
@@ -34,8 +34,8 @@ const projects = [
 		description:
 			"A full-stack tax calculation app with real-time updates. so you can calculate your taxes in a flash.",
 		tech: ["React", "Tailwind", "TypeScript", "Node.js", "MongoDB"],
-		live: "https://taxlator-v2.vercel.app/",
-		github: "https://github.com/ebenezerkingv2/taxlator-v2",
+		live: "https://taxlatorv2.vercel.app/",
+		github: "https://github.com/ebenezerkingv2/taxlator_v2",
 	},
 ];
 // =====================================
