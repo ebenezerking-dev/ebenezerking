3a6ce3f1ab20a85d71f1e37053a3bc960e916ea5
Ebenezer Tiewei <ebenezertiewei@gmail.com>
2026-04-16 21:33:21 +0100

    updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -32,8 +32,6 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.options("*", cors(corsOptions));
-
 // ========================= MIDDLEWARE =========================
 app.use(express.json());
 
