7cbc8cd34822facc7efbc16eba8c614a24543d77
Ebenezer Tiewei <ebenezertiewei@gmail.com>
2026-06-03 01:23:13 +0100

    updates

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import { NavbarProvider } from ".";
+import { NavbarProvider } from "./components/reusables/context/NavbarProvider";
 
 // =====================================
 ReactDOM.createRoot(document.getElementById("root")!).render(
