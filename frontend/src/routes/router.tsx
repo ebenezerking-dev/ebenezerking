e251051312a8904e76b8c2a70a45e7f227a95463
Ebenezer Tiewei <ebenezertiewei@gmail.com>
2026-07-19 17:40:00 +0100

    updated

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -2,13 +2,11 @@
 // this file contains the router configuration for the application
 
 // ================== IMPORTS
-import { lazy, Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import HomePageLayout from "../components/layouts/HomePageLayout";
 import OtherPagesLayout from "../components/layouts/OtherPagesLayout";
 import HomePage from "../pages/HomePage";
-// ================== LAZY LOAD PAGES
-const CareerPage = lazy(() => import("../pages/CareerPage"));
+import CareerPage from "../pages/CareerPage";
 
 // ================== ROUTER
 export const router = createBrowserRouter([
@@ -28,17 +26,7 @@ export const router = createBrowserRouter([
 		children: [
 			{
 				path: "/careers/:career",
-				element: (
-					<Suspense
-						fallback={
-							<div className="flex min-h-screen items-center justify-center">
-								<div className="text-white">Loading...</div>
-							</div>
-						}
-					>
-						<CareerPage />
-					</Suspense>
-				),
+				element: <CareerPage />,
 			},
 		],
 	},
